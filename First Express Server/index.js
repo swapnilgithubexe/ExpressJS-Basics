d51d7962ad93b748dcdc9bad8c2dd99f0a29f459
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-09-06 16:08:45 +0530

    updated v1

--- First Express Server/index.js
+++ First Express Server/index.js
@@ -1,11 +1,9 @@
 
-const express = require("express")
-const server = express()
+const express = require("express");
+const server = express();
 server.get("/", (req, res) => {
   res.send("Be a Coding Ninja.");
 })
-server.listen(4000, () => {
-  console.log(`Server is running on port 4000`)
-})
+
 
 module.exports = { server };
