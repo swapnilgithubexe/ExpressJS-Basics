95a1bf0abeee210145a857ba8487b35ba6239156
swapnilgithubexe <swapnildutta2002@gmail.com>
2024-09-13 01:55:28 +0530

    initial commit

---
+++ Implementing Form Validation - Server Side/blog.controller.js
@@ -0,0 +1,33 @@
+export const validateBlog = (req, res) => {
+  const { title, description, imageUrl } = req.body;
+  let errors = [];
+
+
+  if (!title || title.trim() == "") {
+    errors.push("The title field should not be empty");
+  }
+  if (title.trim().length < 3) {
+    errors.push("The title field should contain at least 3 characters");
+  }
+  if (!description || description.trim() == "") {
+    errors.push("The description field should not be empty");
+  }
+  if (description.trim().length < 10) {
+    errors.push("The description field should contain at least 10 characters");
+  }
+  try {
+    const validUrl = new URL(imageUrl);
+  } catch (error) {
+    errors.push("The image URL provided should be a valid URL");
+  }
+
+  if (errors.length > 0) {
+    return res.status(401).render("addBlog", { errors: errors, success: false });
+  }
+
+  res.status(201).render("addBlog", { errors: [], success: true });
+};
+
+export const renderBlogForm = (req, res) => {
+  res.render("addBlog", { errors: null, success: false });
+};
